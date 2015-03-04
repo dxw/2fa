5de6adc05762e1d434ddc06bd9124b885e87d993
Tom Adams <tom@dxw.com>
2015-03-04 14:09:01 -0500

    Allow changing enabled status for blogs

--- lib/network-sites.php
+++ lib/network-sites.php
@@ -1,5 +1,7 @@
 <?php
 
+// Display blog enabled/disabled status in list
+
 add_filter('wpmu_blogs_columns', function ($columns) {
   $columns['2fa'] = '2FA';
   return $columns;
@@ -12,3 +14,26 @@ add_action('manage_sites_custom_column', function ($column_name, $blawg_id) {
     echo $enabled ? 'Enabled' : 'Disabled';
   }
 }, 10, 2);
+
+// Enabled/disabled status option
+
+add_action('wpmueditblogaction', function ($id) {
+  $enabled = get_option('2fa_enabled') === 'yes';
+  ?>
+
+  <tr class="form-field">
+    <th scope="row">2FA</th>
+    <td><label><input name="2fa_enabled" type="checkbox" value="yes" <?php echo $enabled ? 'checked' : '' ?>> Enabled</label></td>
+  </tr>
+
+  <?php
+});
+
+// Fired after nonce checks
+add_action('wpmu_update_blog_options', function () {
+  if (isset($_POST['2fa_enabled']) && $_POST['2fa_enabled'] === 'yes') {
+    update_option('2fa_enabled', 'yes');
+  } else {
+    update_option('2fa_enabled', 'no');
+  }
+});
