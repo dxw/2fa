4aa431c4896379e3c665d5aec47c0736dbfa0ae9
Tom Adams <tom@dxw.com>
2015-03-06 14:36:59 -0500

    Fix off-by-one error

--- views/setup.php
+++ views/setup.php
@@ -4,7 +4,7 @@ if (!twofa_user_enabled(get_current_user_id())) {
   ?>
   <p>You cannot use 2FA because it has not been set up for your account yet.</p>
   <?php
-} else if (twofa_user_activated(get_current_user_id()) > TWOFA_MAX_DEVICES) {
+} else if (twofa_user_activated(get_current_user_id()) >= TWOFA_MAX_DEVICES) {
   ?>
   <p>You already have the maximum number of devices activated. Please deactivate one before setting up a new device.</p>
   <?php
