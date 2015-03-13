21ad1464f53f094d95f507f091e686d4410bb285
Tom Adams <tom@dxw.com>
2015-03-13 14:34:25 -0400

    Rename

--- 2fa.php
+++ 2fa.php
@@ -15,7 +15,7 @@ require(__DIR__."/lib/model.php");
 require(__DIR__."/lib/assets.php");
 require(__DIR__."/lib/setup.php");
 
-require(__DIR__."/lib/network-sites.php");
+require(__DIR__."/lib/sites-list.php");
 require(__DIR__."/lib/user-profile.php");
 require(__DIR__."/lib/page.php");
 require(__DIR__."/lib/force-setup.php");
