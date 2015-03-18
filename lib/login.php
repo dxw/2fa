0ddd196705bd43d9459069a929fe9d7aa06403d8
Tom Adams <tom@dxw.com>
2015-03-18 14:24:19 -0400

    Allow non-2fa users to log in

--- lib/login.php
+++ lib/login.php
@@ -5,6 +5,36 @@ add_action('login_form_login', function () {
 
   $first_phase = true;
 
+  // Logging in
+
+  if (isset($_POST['log']) && isset($_POST['pwd'])) {
+    // Verify credentials
+    $user = wp_authenticate($_POST['log'], $_POST['pwd']);
+
+    if (is_wp_error($user)) {
+      wp_die('TODO: could not authenticate');
+    }
+
+    if (twofa_user_activated($user->ID)) {
+      // If the user has 2fa activated, send them to phase 2
+
+      $first_phase = false;
+    } else {
+      // If the user has 2fa deactivated, log them in
+
+      $rememberme = isset($_POST['rememberme']);
+      wp_set_auth_cookie($user->ID, $rememberme);
+
+      // TODO: redirect to the correct URL
+      wp_redirect(get_admin_url());
+    }
+
+
+    // Phase 2
+  }
+
+  // Templates
+
   if ($first_phase) {
     // Phase 1 - user/pass form
 
@@ -20,7 +50,7 @@ add_action('login_form_login', function () {
         <input type="text" name="pwd">
       </label>
       <label>
-        <input type="checkbox" name="rememberme">
+        <input type="checkbox" name="rememberme" value="yes">
         Remember Me
       </label>
 
