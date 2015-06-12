eaa4fb0e586f2841240930c2242cc770ac20dbe6
Tom Adams <tom@dxw.com>
2015-06-12 10:02:58 -0400

    Replace string specific to client site
    Fixes: #3

--- lib/settings.php
+++ lib/settings.php
@@ -13,7 +13,7 @@ add_action('network_admin_menu', function () {
     <div class="wrap">
       <h2>2 Factor Authentication</h2>
 
-      <p>This page controls the 2 factor authentication (2FA) settings for the GOV.UK blog platform. It allows you to specify the length of time for which users can choose to skip 2FA on a specific computer.</p>
+      <p>This page controls the 2 factor authentication (2FA) settings for the <?php bloginfo('name') ?> platform. It allows you to specify the length of time for which users can choose to skip 2FA on a specific computer.</p>
 
       <p>The default is 30 days. Do not change this unless there is a specific reason to. Any change will affect all users on the platform that have 2FA enabled.</p>
 
