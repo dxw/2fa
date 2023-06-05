a961c7e82a4cbc10927205769e7e28d1434d111b
Sarah Mount <sarah.mount@dxw.com>
2023-06-05 17:27:28 +0100

    Integration tests used MariaDB
    On Apple M2 machines, the integration tests previously failed with
    the following error:
    
        5.7: Pulling from library/mysql
        docker: no matching manifest for linux/arm64/v8 in the manifest list entries.
    
    Using MariaDB means that these tests are more platform-independent.

--- tests/run-with-docker.sh
+++ tests/run-with-docker.sh
@@ -10,7 +10,7 @@ docker build -t 2fa_test_main tests/
 # Get MySQL running
 docker stop 2fa_test_mysql || true
 docker rm 2fa_test_mysql || true
-docker run -d --name=2fa_test_mysql --health-cmd='mysqladmin ping --silent' -e MYSQL_DATABASE=2fa_test -e MYSQL_ROOT_PASSWORD=foobar mysql:5.7
+docker run -d --name=2fa_test_mysql --health-cmd='mysqladmin ping --silent' -e MYSQL_DATABASE=2fa_test -e MYSQL_ROOT_PASSWORD=foobar mariadb:10.5.8
 export HOST=`docker inspect -f '{{.NetworkSettings.IPAddress}}' 2fa_test_mysql`
 while  STATUS=`docker inspect --format "{{.State.Health.Status}}" 2fa_test_mysql`; [ $STATUS != "healthy" ]; do
     echo "waiting for database"
