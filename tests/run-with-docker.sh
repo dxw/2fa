e1c7a1a37da081da2f7523aba13e346b5a4e2da7
Sarah Mount <sarah.mount@dxw.com>
2023-06-05 17:34:01 +0100

    Shell script now passes shellcheck

--- tests/run-with-docker.sh
+++ tests/run-with-docker.sh
@@ -11,11 +11,12 @@ docker build -t 2fa_test_main tests/
 docker stop 2fa_test_mysql || true
 docker rm 2fa_test_mysql || true
 docker run -d --name=2fa_test_mysql --health-cmd='mysqladmin ping --silent' -e MYSQL_DATABASE=2fa_test -e MYSQL_ROOT_PASSWORD=foobar mariadb:10.5.8
-export HOST=`docker inspect -f '{{.NetworkSettings.IPAddress}}' 2fa_test_mysql`
-while  STATUS=`docker inspect --format "{{.State.Health.Status}}" 2fa_test_mysql`; [ $STATUS != "healthy" ]; do
+HOST=$(docker inspect -f '{{.NetworkSettings.IPAddress}}' 2fa_test_mysql)
+export HOST
+while  STATUS=$(docker inspect --format "{{.State.Health.Status}}" 2fa_test_mysql); [ "$STATUS" != "healthy" ]; do
     echo "waiting for database"
     sleep 1
 done
 
 # Run the tests
-docker run --rm -v `pwd`:/app -e MYSQL_HOST=${HOST} -e MYSQL_PASSWORD=foobar 2fa_test_main sh -c 'bundle install && bundle exec rspec spec/*_spec.rb'
+docker run --rm -v "$(pwd)":/app -e MYSQL_HOST="${HOST}" -e MYSQL_PASSWORD=foobar 2fa_test_main sh -c 'bundle install && bundle exec rspec spec/*_spec.rb'
